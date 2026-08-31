6bdd215f4b408da3168f9394ce7b4970411cdf71
wieon <hfio-r15680@outlook.com>
2026-08-31 21:29:15 +0800

    [task 0-3] complete color method and test

--- sp26-s12345/proj0/src/Particle.java
+++ sp26-s12345/proj0/src/Particle.java
@@ -23,6 +23,20 @@ public class Particle {
     public Color color() {
         if (flavor == ParticleFlavor.EMPTY) {
             return Color.BLACK;
+        } else if (flavor == ParticleFlavor.SAND) {
+            return Color.YELLOW;
+        } else if (flavor == ParticleFlavor.BARRIER) {
+            return Color.GRAY;
+        } else if (flavor == ParticleFlavor.WATER) {
+            return Color.BLUE;
+        } else if (flavor == ParticleFlavor.FOUNTAIN) {
+            return Color.CYAN;
+        } else if (flavor == ParticleFlavor.PLANT) {
+            return new Color(0, 255, 0);
+        } else if (flavor == ParticleFlavor.FIRE) {
+            return new Color(255, 0, 0);
+        } else if (flavor == ParticleFlavor.FLOWER) {
+            return new Color(255, 141, 161);
         }
         return Color.GRAY;
     }
