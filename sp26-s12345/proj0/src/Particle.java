19cee960eaac36501751351fcab22696d234fe13
wieon <hfio-r15680@outlook.com>
2026-08-31 22:03:00 +0800

    [task 4] complete moveInto method

--- sp26-s12345/proj0/src/Particle.java
+++ sp26-s12345/proj0/src/Particle.java
@@ -42,6 +42,11 @@ public class Particle {
     }
 
     public void moveInto(Particle other) {
+        other.flavor = this.flavor;
+        other.lifespan = this.lifespan;
+
+        this.flavor = ParticleFlavor.EMPTY;
+        this.lifespan = -1;
     }
 
     public void fall(Map<Direction, Particle> neighbors) {
