b86359f3ceec066821c0c42fe1a8ccf285f666fc
wieon <hfio-r15680@outlook.com>
2026-09-22 10:57:09 +0800

    proj2 tests

--- sp26-s12345/proj2/tests/ArrayDeque61BEnhancementTest.java
+++ sp26-s12345/proj2/tests/ArrayDeque61BEnhancementTest.java
@@ -5,11 +5,26 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BEnhancementTest {
     @Test
+    /* Test toString method. */
     public void toStringTest() {
         Deque61B<String> ad = new ArrayDeque61B<>();
         ad.addLast("front");
         ad.addLast("middle");
         ad.addLast("back");
         System.out.println(ad);
+    }
+
+    @Test
+    /* Test equals method. */
+    public void equalsTest() {
+        Deque61B<String> ad = new ArrayDeque61B<>();
+        ad.addLast("front");
+        ad.addLast("middle");
+        ad.addLast("back");
+        Deque61B<String> ad2 = new ArrayDeque61B<>();
+        ad2.addLast("front");
+        ad2.addLast("middle");
+        ad2.addLast("back");
+        assertThat(ad.equals(ad2)).isTrue();
     }
 }
